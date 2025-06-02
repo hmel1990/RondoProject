72d0732396ba21bd19b757d7de28dd55ef3ec40f
hmel1990 <hmel408757595@gmail.com>
2025-06-02 12:12:27 +0300

    Form for upload

--- db_connect.php
+++
@@ -1,8 +0,0 @@
-<?php 
-        define('HOST', 'fdb1030.awardspace.net');
-        define('USER', '4621564_db');
-        define('PASS', 'pe4en6ka');
-        define('DB',   '4621564_db');
- 
-        $con = mysqli_connect(HOST,USER,PASS,DB) or die('unable to connect to db');
- ?>
